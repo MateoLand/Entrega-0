a9c7ea4c5df2bfe90c5ea6ccdcaaff918051e386
MateoLand <110478181+MateoLand@users.noreply.github.com>
2022-08-21 20:40:15 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
+    let email = sesionStorage.getItem("mail")
+    if (email == null){
+        alert("Vaya a Loggearse >:(");
+        this.location.href = "login.html";
+    } else {
+        this.document.getElementById("email").innerHTML = email;
+    }
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
@@ -11,4 +20,5 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+    
 });
